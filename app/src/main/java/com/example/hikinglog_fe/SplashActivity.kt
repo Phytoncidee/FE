56e20d2840b5d470c4d2fefcdd48fc4b062e0160
hyom1n <hyomin0806@gmaile.com>
2024-09-11 03:33:59 +0900

    [FEAT] 전체 산 지역별 목록 -> 산 상세 이동

--- app/src/main/java/com/example/hikinglog_fe/SplashActivity.kt
+++ app/src/main/java/com/example/hikinglog_fe/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.hikinglog_fe
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
@@ -18,13 +19,12 @@ class SplashActivity : AppCompatActivity() {
 
         val pref = getSharedPreferences("userToken", MODE_PRIVATE)
         val token = pref.getString("token", null)
+        Log.d("자동로그인 여부 확인", token ?: "토큰이 null입니다.")   // null일 경우 빈 문자열 을 기본값으로 설정
 
         val mainExe : Executor = ContextCompat.getMainExecutor(this)
         val backgroundExe : ScheduledExecutorService = Executors.newSingleThreadScheduledExecutor()
-        backgroundExe.schedule(
-            // 쓰레드, 시간, 단위
-            {
-                mainExe.execute({ // 쓰레드 형태로 실행
+        backgroundExe.schedule({    // 쓰레드, 시간, 단위
+                mainExe.execute {   // 쓰레드 형태로 실행
                     val intent = if (token.isNullOrEmpty()) {
                         Intent(applicationContext, LoginActivity::class.java)
                     } else {
@@ -32,7 +32,7 @@ class SplashActivity : AppCompatActivity() {
                     }
                     startActivity(intent)
                     finish()
-                })
+                }
             },
             3, TimeUnit.SECONDS
         )
--- app/src/main/java/com/example/hikinglog_fe/models/RegionMountainResponse.kt
+++ app/src/main/java/com/example/hikinglog_fe/models/RegionMountainResponse.kt
@@ -15,7 +15,7 @@ data class MountainsByRegion(
     val mntinfdt: String,
     val mntisname: String,
     val mntidetails: String,   // 산 상세 정보
-    val mntilistno: Long,      // 산 목록 번호
+    val mntilistno: String,      // 산 목록 번호
     val mntiadd: String,       // 산의 주소
     val mntiname: String,      // 산의 이름
     val mntitop: String
